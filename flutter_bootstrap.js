b84a33a0aec2ba24814c0809a730be7f44166345
srsakilhossen302 <srsakilhossen302@gmail.com>
2026-07-31 10:59:01 +0600

    Deploy Skip to Main App button and cache-busting web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3371313446" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "589472672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
